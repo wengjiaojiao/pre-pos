3f192c12023d488e077800733b0e464f89d29273
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-17 15:15:34 +0800

    the first

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,12 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
-}
+  var collection=[];
+  for( var i=0;i<collection_a.length;i++){
+       for (var k=0;k<collection_b.length;i++){
+          if(collection_a[i]==collection_b[k]){
+                  collection[]=collection_a[i];
+         }
+       }
+      return collection;
+  }
 
 module.exports = collect_same_elements;
