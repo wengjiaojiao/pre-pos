ac5f7ea5a684c41861c53eb7d490e259a79a241d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-18 15:28:42 +0800

    change the method

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,5 +1,27 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+    var sum_list=[];
+    for(var i=0;i<collection.length;i++) {
+            var summary=1;
+            var name=collection[i].substring(0,1);
+            if(collection[i].indexOf("-")>-1||collection[i].indexOf(":") >-1||collection[i].indexOf("[") >-1) {
+                summary=collection[i].substring(2);
+                summary=parseInt(summary);
+             }
+             var exsit=false;
+             for(var k=0;k<sum_list.length;k++) {
+                 if(sum_list[k].name==name) {
+                          sum_list[k].summary=sum_list[k].summary+summary;
+                          exsit=true;
+                 }
+             }
+             if(!exsit) {
+                    var temp={};
+                    temp.name=name;
+                    temp.summary=summary;
+                    sum_list.push(temp);
+             }
+        }
+        return sum_list;
 }
 
-module.exports = count_same_elements;
+module.exports = count_same_elements
